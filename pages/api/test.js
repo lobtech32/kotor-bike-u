95d3debf193ec868fa7fbc6805a43fb30290de45
lobtech32 <buraksaltali61@gmail.com>
2025-06-20 17:53:05 +0200

    Create test.js

---
+++ pages/api/test.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log("Gelen veri:", req.body);
+    res.status(200).json({ success: true, received: req.body });
+  } else {
+    res.status(405).json({ error: "Sadece POST istekleri kabul edilir." });
+  }
+}
