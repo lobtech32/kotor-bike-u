a3c231126ea3510d0e9cdd2290afd3453e994266
lobtech32 <buraksaltali61@gmail.com>
2025-06-20 18:09:32 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>🚲 Kotor Bike Sistemi Çalışıyor</h1>
+      <p>QR kodla test edebilirsin veya doğrudan /start yolunu aç.</p>
+    </div>
+  );
+}
