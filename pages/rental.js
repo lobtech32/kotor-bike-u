8eb1734c2601ecae1bff634bb80a2c5beb893d07
lobtech32 <buraksaltali61@gmail.com>
2025-06-20 16:17:53 +0200

    Delete pages directory

--- pages/rental.js
+++
@@ -1,3 +0,0 @@
-export default function Rental() {
-  return <div className="p-4 text-xl">Rental Page (placeholder)</div>;
-}
--- pages/start.js
+++
@@ -1,3 +0,0 @@
-export default function Start() {
-  return <div className="p-4 text-xl">Start Page (placeholder)</div>;
-}
