132877e811f9e34eb5c473f299f27e3c0047e9d0
lobtech32 <buraksaltali61@gmail.com>
2025-06-21 12:13:09 +0200

    Create tr.js

---
+++ loca leş/tr.js
@@ -0,0 +1,9 @@
+// locales/tr.js
+export default {
+  bike_info_title: "Bisiklet Bilgileri",
+  battery_status: "Batarya durumu",
+  rental_options: "Kiralama seçenekleri",
+  unlimited_rental: "Sınırsız kullanım",
+  sponsored: "Sponsorlu İçerikler",
+  bike_map_title: "Bisikletlerin Konumu Haritası",
+}
