6d4125cf0591166556e4b53214c2f639febf4ca1
lobtech32 <buraksaltali61@gmail.com>
2025-06-21 15:00:13 +0200

    Update promo.js

--- pages/promo.js
+++ pages/promo.js
@@ -1,16 +1,13 @@
-// pages/promo.js
-import { useRouter } from 'next/router'
 import MapView from '../components/MapView'
 import Ads from '../components/Ads'
 import { useTranslations } from '../lib/useTranslations'
 
 export default function Promo() {
-  const { locale } = useRouter()
-  const t = useTranslations(locale)
+  const t = useTranslations()
 
   return (
     <div className="p-6 max-w-lg mx-auto">
-      <MapView t={t} />
+      <MapView />
       <Ads t={t} />
     </div>
   )
