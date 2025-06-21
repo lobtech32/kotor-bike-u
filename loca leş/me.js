979764f682fc451f9bbb7738df16895dbb60f23f
lobtech32 <buraksaltali61@gmail.com>
2025-06-21 12:37:33 +0200

    Create me.js

---
+++ loca leş/me.js
@@ -0,0 +1,9 @@
+// locales/me.js
+export default {
+  bike_info_title: "Informacije o biciklu",
+  battery_status: "Stanje baterije",
+  rental_options: "Opcije najma",
+  unlimited_rental: "Neograničeno korišćenje",
+  sponsored: "Sponzorisani oglasi",
+  bike_map_title: "Mapa lokacije bicikala",
+}
