1d909cbdafda0c7e7c7096c35d0be7875cb2c086
lobtech32 <buraksaltali61@gmail.com>
2025-06-21 10:50:44 +0200

    Delete pages directory

--- pages/api/test.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    console.log("Gelen veri:", req.body);
-    res.status(200).json({ success: true, received: req.body });
-  } else {
-    res.status(405).json({ error: "Sadece POST istekleri kabul edilir." });
-  }
-}
