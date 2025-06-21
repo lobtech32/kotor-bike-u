ca55c4dbcfa7610da8014c29628754a5eb723806
lobtech32 <buraksaltali61@gmail.com>
2025-06-21 12:35:59 +0200

    Create de.js

---
+++ loca leş/de.js
@@ -0,0 +1,9 @@
+// locales/de.js
+export default {
+  bike_info_title: "Fahrradinformationen",
+  battery_status: "Batteriestatus",
+  rental_options: "Mietoptionen",
+  unlimited_rental: "Unbegrenzte Nutzung",
+  sponsored: "Gesponserte Inhalte",
+  bike_map_title: "Fahrradstandorte Karte",
+}
