9f07974ac54957a78ed0cd24213ee1af1a4b8bdb
lobtech32 <buraksaltali61@gmail.com>
2025-06-21 10:53:54 +0200

    Create lang.js

---
+++ lig/lang.js
@@ -0,0 +1,40 @@
+// lib/lang.js
+const languages = {
+  me: {
+    bikeInfo: "Informacije o biciklu",
+    battery: "Stanje baterije",
+    rentOptions: "Opcije najma",
+    map: "Mapa bicikala",
+    ads: "Preporuke iz okoline",
+  },
+  en: {
+    bikeInfo: "Bike Information",
+    battery: "Battery Status",
+    rentOptions: "Rental Options",
+    map: "Bike Locations Map",
+    ads: "Nearby Recommendations",
+  },
+  ru: {
+    bikeInfo: "Информация о велосипеде",
+    battery: "Уровень заряда",
+    rentOptions: "Варианты аренды",
+    map: "Карта велосипедов",
+    ads: "Рядом с вами",
+  },
+  de: {
+    bikeInfo: "Fahrradinformationen",
+    battery: "Batteriestatus",
+    rentOptions: "Mietoptionen",
+    map: "Fahrradkarte",
+    ads: "Empfehlungen in der Nähe",
+  },
+  tr: {
+    bikeInfo: "Bisiklet Bilgileri",
+    battery: "Pil Durumu",
+    rentOptions: "Kiralama Seçenekleri",
+    map: "Bisiklet Haritası",
+    ads: "Yakındaki Öneriler",
+  },
+}
+
+export default languages
